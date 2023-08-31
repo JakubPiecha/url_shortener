25db24f9c947a0514b208387523c6b0113cb7e92
Jakub Piecha <piechajakub@wp.pl>
2023-08-31 12:29:12 +0200

    configuration flake8 linter for django project

--- api/users/models.py
+++ api/users/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
