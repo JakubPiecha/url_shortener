10aa13f45f0b0c9ec89ca67d414fe77a6d8b8e0d
Jakub Piecha <piechajakub@wp.pl>
2023-09-05 14:24:31 +0200

    create postman colections for local and heroku

--- api/config/settings.py
+++ api/config/settings.py
@@ -11,13 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
 import dj_database_url
 from config.swagger_settings import SPECTACULAR_SETTINGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJ_SECRET_KEY")
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DJ_DEBUG", 0))
 
 ALLOWED_HOSTS = os.environ.get("DJ_ALLOWED_HOSTS", "").split()
 
